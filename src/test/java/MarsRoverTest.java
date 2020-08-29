8f743b377cbc761967ae24bfa6d5770969a227e3
Walter Nuccio <wnuccio@hotmail.com>
2020-08-29 16:00:56 +0200

    refactor test to remove duplicated instantiation of MarsRover

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -5,9 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MarsRoverTest {
 
 
+    private MarsRover marsRover;
+
+    @BeforeEach
+    void setUp() {
+        marsRover = new MarsRover();
+    }
+
     @Test
     public void should_stay_into_start_position_after_empty_command() {
-        MarsRover marsRover = new MarsRover();
         String output = marsRover.execute("");
 
         assertEquals("0:0:N", output);
@@ -15,7 +22,6 @@ public class MarsRoverTest {
 
     @Test
     public void should_advance_one_position_towards_N() {
-        MarsRover marsRover = new MarsRover();
         String output = marsRover.execute("M");
 
         assertEquals("0:1:N", output);
