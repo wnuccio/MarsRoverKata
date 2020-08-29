a5fd8e73785911ef93b0723afd46a68d81e906a4
Walter Nuccio <wnuccio@hotmail.com>
2020-08-29 15:20:34 +0200

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FirstTest {
+
+
+    @Test
+    public void
+    should_fail() {
+        fail();
+    }
+}
