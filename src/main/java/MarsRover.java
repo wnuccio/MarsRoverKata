1d7c912dee336454b2c95fc7190780d19ee1cc5d
Walter Nuccio <wnuccio@hotmail.com>
2020-08-29 16:40:57 +0200

    rotate to left three times

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,8 +1,27 @@
 public class MarsRover {
-    public String execute(String s) {
-        if ("L".equals(s)) return "0:0:W";
-        if ("LL".equals(s)) return "0:0:S";
 
-        return String.format("0:%d:N", s.length());
+
+    public String execute(String s) {
+        String direction;
+        int yPosition;
+
+        if (s.contains("L")) {
+            yPosition = 0;
+            direction = direction(numOfL(s));
+        } else {
+            yPosition = s.length();
+            direction = direction(0);
+        }
+
+        return String.format("0:%d:%s", yPosition, direction);
+    }
+
+    private String direction(int numberOfLs) {
+        String[] directions = new String[]{"N", "W", "S", "E"};
+        return directions[numberOfLs];
+    }
+
+    private int numOfL(String s) {
+        return (int) s.chars().filter(ch -> ch == 'L').count();
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -32,5 +32,7 @@ public class MarsRoverTest {
     void should_rotate_to_left() {
         assertEquals("0:0:W", marsRover.execute("L"));
         assertEquals("0:0:S", marsRover.execute("LL"));
+        assertEquals("0:0:E", marsRover.execute("LLL"));
+//        assertEquals("0:0:N", marsRover.execute("LLLL"));
     }
 }
